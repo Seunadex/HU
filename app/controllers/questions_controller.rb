7d57a723819496955c1519d52e01fddc47bdf2d8
seunadex <seun.adekunle@andela.com>
2017-10-08 15:52:50 +0100

    create answer resource

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,11 @@
 class QuestionsController < ApplicationController
+
+  def show
+    @question = Question.find(params[:id])
+  end
   def create
     Question.create(question_params)
-    
+
     redirect_to root_path
   end
 
